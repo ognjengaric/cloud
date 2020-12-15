13a2048217da465f20f9e8022164ece9455bf8e2
Ognjen <ognjengari@gmail.com>
2020-12-16 00:12:38 +0100

    added to vagrant vm

--- file.sh
+++ file.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
+# Docker Compose
 sudo curl -L --fail https://github.com/docker/compose/releases/download/1.27.4/run.sh -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose up --build
