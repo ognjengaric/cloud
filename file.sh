e950f31773ae413850ad0afdba231686c8cf90f6
Ognjen <ognjengari@gmail.com>
2020-12-15 23:27:01 +0100

    added port mappings

--- file.sh
+++ file.sh
@@ -2,3 +2,6 @@
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+sudo curl -L --fail https://github.com/docker/compose/releases/download/1.27.4/run.sh -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
