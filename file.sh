203de42982f0c454d46b6c3d348eea8a6653b0b1
Ognjen <ognjengari@gmail.com>
2020-12-29 21:35:56 +0100

    workflow test

--- file.sh
+++ file.sh
@@ -4,7 +4,6 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-#test
 # Docker Compose
 sudo curl -L --fail https://github.com/docker/compose/releases/download/1.27.4/run.sh -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
